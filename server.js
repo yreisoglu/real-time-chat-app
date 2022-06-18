ca9aa4d4ab6cc6862e3113e099589157ad4fa42d
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-06-18 18:22:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { getCurrentUser, joinUser, userDisconnect } = require("./user");
 const INDEX = '/index.html';
 app.use(express()).use((req, res) => res.sendFile(INDEX, { root: __dirname }));
 
-const port = 5000;
+const port = process.env.PORT | 5000;
 
 app.use(cors());
 
